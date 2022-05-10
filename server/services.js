2e2e9f9f634f84bd77175b2ed577c05e79979126
Sorexkate <54243342+Sorexkate@users.noreply.github.com>
2022-05-09 23:35:58 -0400

    Update services.js

--- server/services.js
+++ server/services.js
@@ -5,7 +5,7 @@ const mysql = require ('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Royalrepublic1',
+    password: '',
     database: 'soccerndb'
 });
 
